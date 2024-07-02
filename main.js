b5d4d265e2e9de27be298d9c825a641bb509a6bc
Matias Gunsett <matiasgunsett@gmail.com>
2024-07-01 23:23:48 -0300

    PreEntrega3

--- main.js
+++ main.js
@@ -1,204 +1,146 @@
-//! FUNCIONES //
-
-// Función para obtener la fecha actual
-const fechaActual = () => {
-  const hoy = new Date();
+//Funcion fecha actual --
+let fechaActual = () => {
+  let hoy = new Date();
   return hoy.toLocaleDateString();
 };
-// Función de ingreso numérico correcto
-const ingresoValido = (valor) => {
-  while (isNaN(valor) || valor < 0 || valor === "") {
-    valor = Number(prompt("Ingrese valores correctos:"));
-  }
-  return valor;
-};
-// Función de ingreso correcto de usuario y password
-// Usuario -- 'Matias'
-// Contraseña -- '12345'
-const usuarios = () => {
-  let usuario = prompt("Ingrese nombre de Usuario:").toLowerCase();
-  let password = Number(prompt("Ingrese contraseña:"));
-
-  while (isNaN(password) || password < 0 || usuario === "") {
-    alert("Ingrese valores correctos");
-    usuario = prompt("Ingrese nombre de Usuario:").toLowerCase();
-    password = Number(prompt("Ingrese contraseña:"));
-  }
-
-  if (usuario === "matias" && password === 12345) {
-    return usuario;
-  } else {
-    alert("Usuario Desconocido, intente nuevamente");
-    return null;
-  }
-};
-// Función para consulta de productos
-const sectores = () => {
-  let opciones = Number(
-    prompt(
-      "Qué sector de Productos desea consultar? :\n" +
-        "1 -Movilidad y Postura\n" +
-        "2 -Ortesis y Protesis\n" +
-        "3 -Productos Ortopedia\n" +
-        "4 -Salir."
-    )
-  );
-
-  while (isNaN(opciones) || opciones < 1 || opciones > 4) {
-    alert("Los valores ingresados son INCORRECTOS, intente nuevamente");
-    opciones = Number(
-      prompt(
-        "Qué sector de Productos desea consultar? :\n" +
-          "1 -Movilidad y Postura\n" +
-          "2 -Ortesis y Protesis\n" +
-          "3 -Productos Ortopedia\n" +
-          "4 -Salir."
-      )
-    );
-  }
-  return opciones;
-};
-// Función para eliminar mercadería
-const eliminarMercaderia = (mercaderia) => {
-  while (true) {
-    alert(
-      "Mercadería en Stock: \n" +
-        mercaderia.map((m) => `${m.nombre} (${m.stock} en stock)`).join("\n")
-    );
-
-    if (!confirm("¿Desea eliminar mercadería?")) {
-      break;
-    }
-    let borrar = Number(prompt("¿Cuántos elementos desea eliminar?"));
-    if (isNaN(borrar) || borrar <= 0) {
-      alert("Por favor, ingrese un número válido de elementos a eliminar");
-      continue;
-    }
-    let desde_donde = Number(
-      prompt(
-        "Coloque 0 si desea eliminar desde el principio o el índice desde donde eliminar:"
-      )
-    );
-    if (
-      isNaN(desde_donde) ||
-      desde_donde < 0 ||
-      desde_donde >= mercaderia.length
-    ) {
-      alert("Por favor, ingrese un índice válido");
-      continue;
-    }
-
-    const eliminados = mercaderia.splice(desde_donde, borrar);
-    if (eliminados.length === 0) {
-      alert(
-        "No se eliminó ningún elemento. Por favor, verifique los valores ingresados"
-      );
-    } else {
-      alert(
-        "Mercadería eliminada:\n" + eliminados.map((m) => m.nombre).join("\n")
-      );
-    }
-    alert(
-      "Mercadería actualizada en stock:\n" +
-        mercaderia.map((m) => `${m.nombre} (${m.stock} en stock)`).join("\n")
-    );
-  }
-};
-
-// Clase para ingreso de productos
+//Funcion PRODUCTOS --
 class Producto {
   constructor(nombre, precio, stock) {
     this.nombre = nombre;
     this.precio = precio;
     this.stock = stock;
   }
-
   actualizarStock(cantidad) {
-    this.stock += cantidad;
+    this.stock = ++cantidad;
   }
-
   toString() {
-    return `${this.nombre}
-    - Precio: $${this.precio} 
-    - Stock:  ${this.stock}`;
+    return `${this.nombre} - Precio: $${this.precio} - Stock: ${this.stock}`;
   }
 }
 
-//! Inicio del Sistema
-const main = () => {
-  let stock_nuevo = []; // array de Productos a cargar
+//Llamado de elementos del HTML --
+let formulario = document.querySelector("form");
+let inicio = document.querySelector(".titulo");
+let iconos = document.querySelector("#icons");
+let mainContento = document.querySelector("#mainContent");
+let welcomeMessage = document.querySelector("#welcomeMessage");
+let content = document.querySelector("#content");
+let message = document.querySelector("#message");
+let loginError = document.querySelector("#loginError");
+let consultaStock = document.querySelector("#consultaStock");
+let agregarStock = document.querySelector("#agregarStock");
+let quitarStock = document.querySelector("#quitarStock");
+let salirStock = document.querySelector("#salirStock");
 
-  let saludo = confirm("Bienvenido a Ortopedia Kinen! Ingresamos?");
-  let usuario;
+let stockNuevo = JSON.parse(localStorage.getItem("stockNuevo")) || [];
 
-  while (saludo) {
-    usuario = usuarios();
-    if (usuario) {
-      alert(`Bienvenido:  ${usuario}  al sistema de stock KINEN`);
-      break;
-    } else {
-      saludo = confirm("¿Desea intentar nuevamente?");
-      if (!saludo) {
-        alert("Hasta pronto!");
-        return;
-      }
-    }
+//Carga de Usuarios a LocalStorage --
+let usuariosCargados = [
+  { username: "sucursal1", password: "kinen1" },
+  { username: "sucursal2", password: "kinen2" },
+  { username: "sucursal3", password: "kinen3" },
+];
+let usuariosValidos = JSON.stringify(usuariosCargados);
+localStorage.setItem("usuarios", usuariosValidos);
+
+//Validacion de Usuario & Pass --
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let usuario = formulario[0].value.toLowerCase();
+  let pass = formulario[1].value;
+  let usuarios = JSON.parse(usuariosValidos);
+
+  let usuarioValido = usuarios.find(
+    (user) => user.username === usuario && user.password === pass
+  );
+
+  if (usuarioValido) {
+    inicio.style.display = "none";
+    mainContento.style.display = "flex";
+    iconos.style.display = "none";
+    Swal.fire({
+      title: `Bienvenido  ${usuario} !`,
+      icon: "success",
+    });
+    localStorage.setItem("logueado", JSON.stringify(usuarioValido)); //Guardamos Logueo
+  } else {
+    loginError.textContent = "Usuario Desconocido, intente nuevamente";
+    loginError.style.display = "block";
   }
+});
 
-  while (true) {
-    let opcion = Number(
-      prompt(
-        `Usuario: ${usuario} - Fecha: ${fechaActual()} \nSeleccione una opción: \n1- Consultar Stock \n2- Cargar Productos \n3- Eliminar Productos \n4- Salir`
-      )
-    );
-    opcion = ingresoValido(opcion);
-
-    if (opcion === 1) {
-      alert(
-        "Stock actual en fecha " +
-          fechaActual() +
-          " :\n" +
-          stock_nuevo.map((producto) => producto.toString()).join("\n")
-      );
-    } else if (opcion === 2) {
-      let nombre = prompt("Ingrese nombre del producto:").toLowerCase();
-      let precio = ingresoValido(Number(prompt("Ingrese precio:")));
-      let stock = ingresoValido(Number(prompt("Ingrese cantidad en stock:")));
-
-      stock_nuevo.push(new Producto(nombre, precio, stock));
-    } else if (opcion === 3) {
-      eliminarMercaderia(stock_nuevo);
-    } else if (opcion === 4) {
-      if (confirm("Desea salir del sistema?")) {
-        break;
-      }
-    } else {
-      alert("Opción no válida, intente nuevamente");
-    }
+//Stock ---
+consultaStock.addEventListener("click", () => {
+  if (stockNuevo.length === 0) {
+    Swal.fire({
+      title: `Stock actual en fecha ${fechaActual()}: 0 `,
+      icon: "info",
+    });
+  } else {
+    let ul = document.createElement("ul");
+    stockNuevo.forEach((producto) => {
+      let li = document.createElement("li");
+      li.textContent = producto.toString();
+      ul.appendChild(li);
+    });
+    content.appendChild(ul);
   }
+  message.style.display = "none";
+});
+
+agregarStock.addEventListener("click", () => {
+  content.style.display = "flex";
+  mainContento.style.display = "none";
+  let addProductForm = document.getElementById("addProductForm");
+  addProductForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let nombre = document.querySelector("#nombreP").value.toLowerCase();
+    let precio = Number(document.querySelector("#precioP").value);
+    let stock = Number(document.querySelector("#stockP").value);
+
+    if (nombre && precio >= 0 && stock >= 0) {
+      stockNuevo.push(new Producto(nombre, precio, stock));
+      localStorage.setItem("stockNuevo", JSON.stringify(stockNuevo));
+      showMessage("Producto agregado con éxito", "info");
+      content.innerHTML = "";
+    } else {
+      showMessage("Por favor, ingrese valores válidos", "error");
+    }
+  });
+});
+
+quitarStock.addEventListener("click", () => {
+  content.innerHTML = `
+            <h3>Eliminar Productos</h3>
+            <p>Seleccione un producto para eliminar:</p>
+            <ul id="productList"></ul>
+        `;
+  let productList = document.getElementById("productList");
+  stockNuevo.forEach((producto, index) => {
+    let li = document.createElement("li");
+    li.textContent = producto.nombre;
+    li.addEventListener("click", () => {
+      stockNuevo.splice(index, 1);
+      localStorage.setItem("stockNuevo", JSON.stringify(stockNuevo));
+      showMessage("Producto eliminado con éxito", "info");
+      showRemoveProductFormBtn.click(); // Refrescar la lista
+    });
+    productList.appendChild(li);
+  });
+});
+
+salirStock.addEventListener("click", () => {
+  mainContent.style.display = "none";
+  incio.style.display = "flex";
+  message.style.display = "none";
+});
+
+let showMessage = (msg, type) => {
+  message.textContent = msg;
+  message.className = type === "error" ? "error-message" : "info-message";
+  message.style.display = "block";
+  setTimeout(() => {
+    message.style.display = "none";
+  }, 3000);
 };
-
-// Ejecutar la función principal
-main();
-
-//! Mostrar Productos de menor a mayor en $$ :
-// productos.sort((a, b) => a.precio - b.precio); // Corregir función de comparación // de + a - en precios
-
-// console.log(productos);
-
-//!recorrer un array:
-// let numeros = [5, 4, 3, 2, 1];
-
-// let dobles = [];
-
-// numeros.forEach((elm) => {
-// Asi recorremos el array al igual q con el 'for' //
-//   dobles.push(elm * 2);
-// });
-
-//! otro recorrido de array:
-// if (productos.map((elm) => elm.precio > 5000)) {
-//   alert("Tiene un descuento del -10%");
-// } else {
-//   alert("No llega al tope min para descuentos,lo siento");
-// }
