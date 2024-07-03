affd2649307203c03af5090bed08c6360474aeb5
Matias Gunsett <matiasgunsett@gmail.com>
2024-07-02 23:56:03 -0300

    PreEntrega3

--- main.js
+++ main.js
@@ -6,34 +6,34 @@ let fechaActual = () => {
 //Funcion PRODUCTOS --
 class Producto {
   constructor(nombre, precio, stock) {
-    this.nombre = nombre;
+    this.nombre = nombre.toUpperCase();
     this.precio = precio;
     this.stock = stock;
   }
   actualizarStock(cantidad) {
     this.stock = ++cantidad;
   }
-  toString() {
-    return `${this.nombre} - Precio: $${this.precio} - Stock: ${this.stock}`;
-  }
+  toString = () =>
+    `${this.nombre} - Precio: $${this.precio} - Stock: ${this.stock}`;
 }
-
+// Convertir PRODUCTOS para HTML --
+let convertirAProducto = (obj) => {
+  return new Producto(obj.nombre, obj.precio, obj.stock);
+};
 //Llamado de elementos del HTML --
 let formulario = document.querySelector("form");
 let inicio = document.querySelector(".titulo");
 let iconos = document.querySelector("#icons");
-let mainContento = document.querySelector("#mainContent");
-let welcomeMessage = document.querySelector("#welcomeMessage");
+let mainContent = document.querySelector("#mainContent");
 let content = document.querySelector("#content");
-let message = document.querySelector("#message");
+let mensaje = document.querySelector("#mensaje");
 let loginError = document.querySelector("#loginError");
 let consultaStock = document.querySelector("#consultaStock");
 let agregarStock = document.querySelector("#agregarStock");
 let quitarStock = document.querySelector("#quitarStock");
 let salirStock = document.querySelector("#salirStock");
 let content2 = document.querySelector("#content2");
-
-let stockNuevo = JSON.parse(localStorage.getItem("stockNuevo")) || [];
+let button = document.querySelector("#botonVolver");
 
 //Carga de Usuarios a LocalStorage --
 let usuariosCargados = [
@@ -58,7 +58,7 @@ formulario.addEventListener("submit", (e) => {
 
   if (usuarioValido) {
     inicio.style.display = "none";
-    mainContento.style.display = "flex";
+    mainContent.style.display = "flex";
     iconos.style.display = "none";
     Swal.fire({
       title: `Bienvenido  ${usuario} !`,
@@ -72,6 +72,9 @@ formulario.addEventListener("submit", (e) => {
 });
 
 //Stock ---
+let stockNuevo = JSON.parse(localStorage.getItem("stockNuevo")) || [];
+stockNuevo = stockNuevo.map(convertirAProducto);
+
 consultaStock.addEventListener("click", () => {
   if (stockNuevo.length === 0) {
     Swal.fire({
@@ -82,18 +85,24 @@ consultaStock.addEventListener("click", () => {
     let ul = document.createElement("ul");
     stockNuevo.forEach((producto) => {
       let li = document.createElement("li");
-      li.textContent = productos.toString();
+      li.textContent = producto.toString();
       ul.appendChild(li);
     });
+    content2.textContent = "";
+    content2.innerHTML = `<h3>Stock General</h3>`;
     content2.appendChild(ul);
     content.style.display = "none";
     content2.style.display = "flex";
+    setTimeout(() => {
+      content2.style.display = "none";
+    }, 5000);
   }
 });
 
+// Sumar stock --
 agregarStock.addEventListener("click", () => {
   content.style.display = "flex";
-  mainContento.style.display = "none";
+  mainContent.style.display = "none";
   let addProductForm = document.querySelector("#addProductForm");
   addProductForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -108,7 +117,17 @@ agregarStock.addEventListener("click", () => {
         title: `Producto agregado con Éxito`,
         icon: "success",
       });
-      content2.innerHTML = "";
+      content2.innerHTML = " ";
+      content2.innerHTML = `<h3>Stock General</h3>`;
+      stockNuevo.forEach((producto) => {
+        const productItem = document.createElement("li");
+        productItem.textContent = producto.toString();
+        content2.appendChild(productItem);
+      });
+      content2.style.display = "flex";
+      setTimeout(() => {
+        content2.style.display = "none";
+      }, 5000);
     } else {
       Swal.fire({
         title: `Ingrese valores Correctos`,
@@ -116,39 +135,63 @@ agregarStock.addEventListener("click", () => {
       });
     }
   });
-});
-
-quitarStock.addEventListener("click", () => {
-  content.innerHTML = `
-            <h3>Eliminar Productos</h3>
-            <p>Seleccione un producto para eliminar:</p>
-            <ul id="productList"></ul>
-        `;
-  let productList = document.getElementById("productList");
-  stockNuevo.forEach((producto, index) => {
-    let li = document.createElement("li");
-    li.textContent = producto.nombre;
-    li.addEventListener("click", () => {
-      stockNuevo.splice(index, 1);
-      localStorage.setItem("stockNuevo", JSON.stringify(stockNuevo));
-      showMessage("Producto eliminado con éxito", "info");
-      showRemoveProductFormBtn.click(); // Refrescar la lista
-    });
-    productList.appendChild(li);
+  button.addEventListener("click", () => {
+    content.style.display = "none";
+    mainContent.style.display = "flex";
   });
 });
 
+// Restar Stock --
+quitarStock.addEventListener("click", () => {
+  content.style.display = "flex";
+  mainContent.style.display = "none";
+  content.textContent = " ";
+  content.innerHTML = `
+            <h3>Eliminar Productos</h3>
+            <p class='eliminarP'>Seleccione un producto para eliminar:</p>
+            <ul id="productList" class="listaE"></ul>
+            <button type="button" class="btn btn-success botonAgregar" id="botonVolver2">Volver</button>`;
+
+  let productList = document.querySelector("#productList");
+  let button2 = document.querySelector("#botonVolver2");
+  function eliminarProducto(index, li) {
+    stockNuevo.splice(index, 1);
+    localStorage.setItem("stockNuevo", JSON.stringify(stockNuevo));
+    productList.removeChild(li);
+    showMessage("Producto eliminado con éxito", "info");
+  }
+
+  stockNuevo.forEach((producto, index) => {
+    let li = document.createElement("li");
+    li.textContent = producto.nombre;
+    li.classList.add("listaE");
+    li.addEventListener("click", () => {
+      eliminarProducto(index, li);
+    });
+    productList.appendChild(li);
+  });
+  button2.addEventListener("click", () => {
+    content.style.display = "none";
+    mainContent.style.display = "flex";
+  });
+});
+
+// Salir del Sistema --
 salirStock.addEventListener("click", () => {
   mainContent.style.display = "none";
-  incio.style.display = "flex";
+  inicio.style.display = "block";
+  iconos.style.display = "flex";
   message.style.display = "none";
 });
 
 let showMessage = (msg, type) => {
-  message.textContent = msg;
-  message.className = type === "error" ? "error-message" : "info-message";
-  message.style.display = "block";
+  mensaje.textContent = msg;
+  mensaje.className = type === "error" ? "error-message" : "info-message";
+  mensaje.style.display = "block";
   setTimeout(() => {
-    message.style.display = "none";
+    mensaje.style.display = "none";
   }, 3000);
 };
+
+//! Profe trate de darle funcionalidad y valor al 'mensaje' pero no lo logre,
+//! quise hace algo distito a las alertas previas pero no pude. Si me da una mano con eso se lo agradezco !
